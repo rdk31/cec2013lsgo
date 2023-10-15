eba7a82a384b9565dbab631eb774126085b7b57c
rdk31 <rdk31@protonmail.com>
2023-10-15 17:52:50 +0200

    f13, f14 partials

--- cec2013lsgo/F13.cpp
+++ cec2013lsgo/F13.cpp
@@ -1,76 +1,91 @@
 #include "F13.h"
 #include <stdio.h>
 
-F13::F13():Benchmarks(){
-  m_havenextGaussian=0;
+F13::F13() : Benchmarks()
+{
+  m_havenextGaussian = 0;
   Ovector = NULL;
   minX = -100;
   maxX = 100;
   ID = 13;
   s_size = 20;
-  dimension = 905;        // because of overlapping
+  dimension = 905; // because of overlapping
   overlap = 5;
   anotherz = new double[dimension];
+  partials_num = s_size;
 }
 
-F13::~F13(){
+F13::~F13()
+{
   delete[] Ovector;
   delete[] Pvector;
   delete[] anotherz;
   for (int i = 0; i < 25; ++i)
-    {
-      delete[] r25[i];
-    }
+  {
+    delete[] r25[i];
+  }
   for (int i = 0; i < 50; ++i)
-    {
-      delete[] r50[i];
-    }
+  {
+    delete[] r50[i];
+  }
   for (int i = 0; i < 100; ++i)
-    {
-      delete[] r100[i];
-    }
+  {
+    delete[] r100[i];
+  }
   delete[] r25;
   delete[] r50;
   delete[] r100;
   delete[] s;
   delete[] w;
-
+  if (partials != NULL)
+  {
+    delete[] partials;
+  }
 }
 
-double F13::compute(double*x){
+double F13::compute(double *x)
+{
   int i;
-  double result=0.0;
-        
-  if(Ovector==NULL)
-    {
-      Ovector = readOvector();
-      Pvector = readPermVector();
-      r25 = readR(25);
-      r50 = readR(50);
-      r100 = readR(100);
-      s = readS(s_size);
-      w = readW(s_size);
-    }
-  
-  for(i=0;i<dimension;i++)
-    {
-      anotherz[i]=x[i]-Ovector[i];
-    }
+  double result = 0.0;
+
+  if (Ovector == NULL)
+  {
+    Ovector = readOvector();
+    Pvector = readPermVector();
+    r25 = readR(25);
+    r50 = readR(50);
+    r100 = readR(100);
+    s = readS(s_size);
+    w = readW(s_size);
+  }
+
+  if (partials != NULL)
+  {
+    delete[] partials;
+  }
+
+  partials = new double[partials_num];
+
+  for (i = 0; i < dimension; i++)
+  {
+    anotherz[i] = x[i] - Ovector[i];
+  }
   // cout<<"o"<<endl;
 
   // s_size non-separable part with rotation
   int c = 0;
   for (i = 0; i < s_size; i++)
-    {
-      // cout<<"c="<<c<<", i="<<i<<endl;
-      anotherz1 = rotateVectorConform(i, c);
-      // cout<<"done rot"<<endl;
-      result += w[i] * schwefel(anotherz1, s[i]);
-      delete []anotherz1;
-      // cout<<result<<endl;
-    }
+  {
+    // cout<<"c="<<c<<", i="<<i<<endl;
+    anotherz1 = rotateVectorConform(i, c);
+    // cout<<"done rot"<<endl;
+    double tmp = w[i] * schwefel(anotherz1, s[i]);
+    partials[i] = tmp;
+    result += tmp;
+    delete[] anotherz1;
+    // cout<<result<<endl;
+  }
 
   update(result);
-  return(result);
+  return (result);
 }
-
